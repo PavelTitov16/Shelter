fab8519f9424c94099357bd5ca1961ba5fa294d6
PavelTitov16 <pashoktitov16@gmail.com>
2022-04-18 22:49:16 +0300

    feat: add new logo for adaptive menu

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -3,15 +3,18 @@ const adaptiveMenu = document.querySelector('.header-menu');
 const nav = document.querySelector('.header-nav');
 const navLinks = document.querySelectorAll('.header-nav__link');
 const shdwBox = document.querySelector('.shadow-box');
+/*const headerLogo = document.querySelector('.header-logo.active');*/
 
 adaptiveMenu.addEventListener('click', () => {
     adaptiveMenu.classList.toggle('active');
     nav.classList.toggle('active');
     shdwBox.classList.toggle('active');
+
 });
 
 navLinks.forEach(navLink => navLink.addEventListener('click', () => {
     adaptiveMenu.classList.remove('active');
     nav.classList.remove('active');
     shdwBox.classList.remove('active');
+
 }));
